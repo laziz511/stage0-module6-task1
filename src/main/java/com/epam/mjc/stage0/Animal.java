f074ad7e001e5b519d3c060f37716ddb83b39d13
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-09-03 08:49:37 +0500

    Done

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,28 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
-
+  private String color;
+  private int numberOfPaws;
+  private boolean hasFur;
+  
+  public Animal(String color,int numberOfPaws,boolean hasFur){
+    this.color = color;
+    this.numberOfPaws = numberOfPaws;
+    this.hasFur = hasFur;
+  }
+  
+  public String getDescription(){
+    if(hasFur){
+      if(numberOfPaws == 1){
+        return "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + " paw and a fur.";
+      }
+      return "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + " paws and a fur.";
+    }
+    if(numberOfPaws == 1){
+      return "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + " paw and no fur.";
+    }
+    return "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + " paws and no fur.";
+  }
+  
+  
 }
